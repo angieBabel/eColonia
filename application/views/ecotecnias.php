d28781372340a5fa27f6e041d9e4bf808a4b32a1
Angelica Barreda B <anjelyk.b@gmail.com>
2015-06-02 07:44:27 -0500

    Aplicación de Css
    Restructuración del diseño de ecotecnias

--- application/views/ecotecnias.php
+++ application/views/ecotecnias.php
@@ -86,10 +86,10 @@
                   </table>
               </td>
               <!--Mapa-->
-              <td padding-left="10px">
-                <section align="right" inline>
+              <td>
+                <div class="col-lg-12">
                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.432986595572!2d-103.72417700000001!3d19.263528000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84255aca2f63ad7b%3A0x1eb2855b1222447e!2sInstituto+Tecnol%C3%B3gico+de+Colima%2C+Sal%C3%B3n+de+La+Paz!5e0!3m2!1ses-419!2smx!4v1432270882803" width="500" height="500" frameborder="0" style="border:0"></iframe>
-                </section>
+                </div>
               </td>
             </tr>
           </table>
