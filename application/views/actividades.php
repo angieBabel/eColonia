2df4054bc342ba20bae1180ed211ef7db091c5e7
Angelica Barreda B <anjelyk.b@gmail.com>
2015-06-03 10:00:02 -0500

    Modificaciones a agregar actividades
    Mejora del diseño

--- application/views/actividades.php
+++ application/views/actividades.php
@@ -7,60 +7,53 @@
     <div class="row" id="r1">
       <h1 class="r1">Agregar una actividad</h1>
     </div>
-    <br>
     <div class="row" id="r2">
-      <div class="col-lg-1"></div>
-      <div class="col-lg-10">
-        <h2>Evento</h2>
-        <form class="form-horizontal">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8">
+        <h2>Actividad</h2>
+        <form class="form-group">
           <div class="form-group col-lg-12">
-            <label for="Nombre" class="col-lg-2 control-label">Nombre</label>
-            <div class="col-lg-10">
-              <input type="text" name="Nombre" class="form-control" placehoder="Nombre de la actividad">
-            </div>
+            <label for="Nombre" class="control-label">Nombre</label>
+            <input type="text" name="Nombre" class="form-control" placehoder="Nombre de la actividad">
+          </div>
+          <div class="form-group col-lg-6">
+            <label for="encargado" class="control-label">Encargado</label>
+            <input type="text" name="encargado" class="form-control" placehoder="Encargado de la actividad">
+          </div>
+          <div class="form-group col-lg-6">
+            <label for="lugar" class="control-label">Lugar</label>
+            <input type="text" name="lugar" class="form-control" placehoder="Calle #, Colonia">
+          </div>
+          <div class="form-group col-lg-5">
+            <label for="hora" class="control-label">Horario</label>
+            <input type="timer" name="hora" class="form-control" placehoder="--:--">
+          </div>
+          <div class="form-group col-lg-5">
+            <label for="fecha" class="control-label">Fecha</label>
+            <input type="timer" name="fecha" class="form-control" placehoder="dd/mm/aaaa">
+          </div>
+          <div class="form-group col-lg-2">
+            <label for="ecobonos" class="control-label">Ecobonos</label>
+            <select class="form-control">
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
           </div>
           <div class="form-group col-lg-12">
-            <label for="representante" class="col-lg-2 control-label">Encargado</label>
-            <div class="col-lg-10">
-              <input type="text" name="representante" class="form-control" placehoder="Encargado de la actividad">
-            </div>
+            <label for="descripcion" class="control-label">Descripción</label>
+            <textarea name="descripcion" id="" cols="30" rows="10" class="form-control" placehoder="Descripción de la actividad"></textarea>
           </div>
           <div class="form-group col-lg-12">
-            <label for="lugar" class="col-sm-2 control-label">Lugar</label>
-            <div class="col-sm-10">
-              <input type="text" name="lugar" class="form-control" placehoder="Calle #, Colonia">
-            </div>
-          </div>
-          <div class="form-group col-lg-12">
-            <label for="hora" class="col-sm-2 control-label">Horario</label>
-            <div class="col-sm-10">
-              <input type="timer" name="hora" class="form-control" placehoder="--:--">
-            </div>
-          </div>
-          <div class="form-group col-lg-12">
-            <label for="fecha" class="col-sm-2 control-label">Fecha</label>
-            <div class="col-sm-10">
-              <input type="timer" name="fecha" class="form-control" placehoder="dd/mm/aaaa">
-            </div>
-          </div>
-          <div class="form-group col-lg-12">
-            <label for="descripcion" class="col-sm-2 control-label">Descripción</label>
-            <div class="col-sm-10">
-              <textarea name="descripcion" id="" cols="30" rows="10" class="form-control" placehoder="Descripción de la actividad"></textarea>
-            </div>
-          </div>
-          <div class="form-group col-lg-12">
-            <label for="ecobonos" class="col-sm-2 control-label">Ecobonos</label>
-            <div class="col-sm-10">
-              <input type="text" name="ecobonos" class="form-control" placehoder="Ecobonos">
-            </div>
-          </div>
-          <div class="form-group col-lg-12">
-            <button>Guardar</button>
+            <div class="col-lg-10"></div>
+            <div class="col-lg-2"><button class="btn btn-success">Guardar</button></div>
           </div>
         </form>
       </div>
-      <div class="col-lg-1"></div>
+      <div class="col-lg-2"></div>
     </div>
   </div>
 </section>
@@ -68,3 +61,4 @@
 <?php
     include_once "/Sections/footer.php";
 ?>
+
