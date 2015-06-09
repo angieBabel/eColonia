f6a071e513c5eb48b8feae25ff6f21adafc7e444
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-06-09 00:14:12 -0500

    agregar nuevo taller

--- application/controllers/uploader.php
+++ application/controllers/uploader.php
@@ -38,36 +38,48 @@ public function altaActEvento(){
 
 
     $this->m_eColonia->altaActEvento($nombre,$representante,$hora,$fecha,$lugar,$descripcion,$ecobonos,$galeria);
-    $this->load->view('welcome/ambActividades');
+    $data = array(
+          'eventos'=>$this->m_eColonia->get_actividadesEvento(),
+          'talleres'=>$this->m_eColonia->get_actividadesTaller(),
+          'ecotecnias'=>$this->m_eColonia->get_actividadesEcotecnias(),
+        );
+      $this->load->view('agenda_actividades',$data);
   }
 
 public function altaActTaller(){
-    $nombre=$this->input->POST('Nombre');
-    $encargado=$this->input->POST('encargado');
-    $instructor=$this->input->POST('responsable');
-    $lugar=$this->input->POST('lugar');
+   $nombre=$this->input->POST('Nombre');
+    $representante=$this->input->POST('encargado');
+    $instructor=$this->input->POST('instructor');
+    $hora=$this->input->POST('hora');
     $ecobonos=$this->input->POST('ecobonos');
-    $lugar=$this->input->POST('idEcotecnia');
+    $ecotecnias=$this->input->POST('ecotecnia')
+    $fecha_inicio=$this->input->POST('fecha-inicio');
+    $fecha_fin=$this->input->POST('fecha-fin');
+    $lugar=$this->input->POST('lugar');
+    $descripcion=$this->input->POST('descripcion');
     $galeria= $this->m_eColonia->agregarGaleria();
 
 
-    $this->m_eColonia->altaActTaller($nombre,$encargado,$instructor,$hora,$fecha,
-      $lugar,$costo,$descripcion,$ecobonos,$galeria);
+    $this->m_eColonia->altaActTaller($nombre,$encargado,$encargado,$instructor,$lugar,
+    $ecobonos,$ecotecnia,$hora,$fecha_inicio,$fecha_fin,$descripcion,$galeria);
     $this->load->view('welcome/ambTaller');
   }
 
 public function altaActEcotecnia(){
     $nombre=$this->input->POST('Nombre');
-    $representante=$this->input->POST('representante');
+    $representante=$this->input->POST('encargado');
     $hora=$this->input->POST('hora');
-    $fecha=$this->input->POST('fecha');
+    $ecobonos=$this->input->POST('ecobonos');
+    $ecotecnias=$this->input->POST('ecotecnia')
+    $fecha_inicio=$this->input->POST('fecha-inicio');
+    $fecha_fin=$this->input->POST('fecha-fin');
     $lugar=$this->input->POST('lugar');
     $descripcion=$this->input->POST('descripcion');
-    $ecobonos=$this->input->POST('ecobonos');
     $galeria= $this->m_eColonia->agregarGaleria();
 
 
-    $this->m_eColonia->altaEvento($nombre,$representante,$hora,$fecha,$lugar,$descripcion,$ecobonos,$galeria);
+    $this->m_eColonia->altaEvento($nombre,$encargado,$encargado,$lugar,
+    $ecobonos,$ecotecnia,$hora,$fecha_inicio,$fecha_fin,$descripcion);
     $this->load->view('welcome/ambEcotecnia');
   }
 //Alta de nueva ecotécnia
